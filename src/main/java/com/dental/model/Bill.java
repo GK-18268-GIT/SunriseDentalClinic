1f0563836ca0b9ab7dce10e9489f4ff6a0a0c87e
GK-18268-GIT <kadg.kavinda2001@gmail.com>
2026-09-24 06:51:45 +0530

    Implemented the model files of the project.

--- src/main/java/com/dental/model/Bill.java
+++ src/main/java/com/dental/model/Bill.java
@@ -1,5 +1,71 @@
 package com.dental.model;
 
 public class Bill {
+	private int id;
+	private String appointmentNumber;
+	private double consultationFee;
+	private double treatmentCost;
+	private double totalAmount;
 	
+	public Bill() {
+		
+	}
+	
+	public Bill(int id, String appointmentNumber, double consultationFee, double treatmentCost, double totalAmount) {
+		this.id = id;
+		this.appointmentNumber = appointmentNumber;
+		this.consultationFee = consultationFee;
+		this.treatmentCost = treatmentCost;
+		this.totalAmount = totalAmount;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getAppointmentNumber() {
+		return appointmentNumber;
+	}
+
+	public void setAppointmentNumber(String appointmentNumber) {
+		this.appointmentNumber = appointmentNumber;
+	}
+
+	public double getConsultationFee() {
+		return consultationFee;
+	}
+
+	public void setConsultationFee(double consultationFee) {
+		this.consultationFee = consultationFee;
+	}
+
+	public double getTreatmentCost() {
+		return treatmentCost;
+	}
+
+	public void setTreatmentCost(double treatmentCost) {
+		this.treatmentCost = treatmentCost;
+	}
+
+	public double getTotalAmount() {
+		return totalAmount;
+	}
+
+	public void setTotalAmount(double totalAmount) {
+		this.totalAmount = totalAmount;
+	}
+	
+	@Override
+    public String toString() {
+        return "Bill{" +
+               "appointment='" + appointmentNumber + '\'' +
+               ", consultation=" + consultationFee +
+               ", treatment="    + treatmentCost   +
+               ", total="        + totalAmount      +
+               '}';
+    }
 }
--- src/main/java/com/dental/model/Dentist.java
+++ src/main/java/com/dental/model/Dentist.java
@@ -1,6 +1,28 @@
 package com.dental.model;
 
 public class Dentist extends Person {
+	private String specialization;
 	
+	public Dentist() {
+		
+	}
+	
+	public Dentist(String specialization, String name, String contactNumber, String address, int age) {
+		super(name, contactNumber, address, age);
+		this.specialization = specialization;
+	}
+
+	public String getSpecialization() {
+		return specialization;
+	}
+
+	public void setSpecialization(String specialization) {
+		this.specialization = specialization;
+	}
+	
+	@Override
+	public String getRole() {
+		return "Dentist";
+	}
 	
 }
